d4c24b9003f012f69e04d454c02d65ecc9bfa09b
yanomaly <family.tolstoy@gmail.com>
2022-02-18 19:08:41 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Sueta"
+
+version := "0.1"
+
+scalaVersion := "3.1.1"
---
+++ src/main/scala/Sueta.scala
@@ -0,0 +1,8 @@
+object Sueta extends App {
+  print("Input argiment (in rad.): ")
+  var x = System.in.read().toDouble
+  print("Input scale: ")
+  var scale = System.in.read().toDouble
+  var cos: Double
+  
+}
