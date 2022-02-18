e1ce30099d9b26f5c69e03c619a6e73e75e42d41
yanomaly <family.tolstoy@gmail.com>
2022-02-18 21:01:36 +0300

    Cosinus on scala

---
+++ src/main/scala/Cos.scala
@@ -0,0 +1,43 @@
+import scala.language.postfixOps
+
+object Cos extends App {
+  print("Input argiment (in rad.) near zero: ")
+  val x = scala.io.StdIn.readDouble() % (2 * Math.PI)
+  print("Input scale (numbers afler dot): ")
+  val digits = scala.io.StdIn.readInt()
+  val scale = Math.pow(10, -digits)
+  val out = "%." + digits + "f"
+
+  var counter = 1
+  var counter2 = 1
+  var cos1 = 1.0
+  var cos2 = 1.0
+  var sc = 1.0
+
+  val factor: Int => Int = (x: Int) =>
+    if(x == 0) 1
+    else x * factor(x - 1)
+
+  while(Math.abs(sc) > scale)
+    sc *= Math.pow(x, 2) / (counter * (counter + 1))
+    cos1 += sc * Math.pow(-1, counter2)
+    counter += 2
+    counter2 += 1
+
+  counter = 2
+  counter2 = 1
+  sc = 1.0
+
+  while(Math.abs(sc) > scale)
+    sc = Math.pow(x, counter) * Math.pow(-1, counter2) / factor(counter)
+    cos2 += sc
+    counter += 2
+    counter2 += 1
+  end while
+
+  printf(out, cos1)
+  println()
+  printf(out, cos2)
+  println()
+  printf(out, Math.cos(x))
+}
--- src/main/scala/Sueta.scala
+++
@@ -1,8 +0,0 @@
-object Sueta extends App {
-  print("Input argiment (in rad.): ")
-  var x = System.in.read().toDouble
-  print("Input scale: ")
-  var scale = System.in.read().toDouble
-  var cos: Double
-  
-}
